e520b15e38b9a31bc2f652874dfa8d8087d44090
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-03 16:19:58 +0300

    feat:[BMF-1][Vadim]: users entity oluşturuldu ve database scheması eklendi

---
+++ src/main/java/com/Bloomify/model/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.Bloomify.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+
+@MappedSuperclass
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+}
---
+++ src/main/java/com/Bloomify/model/User.java
@@ -0,0 +1,33 @@
+package com.Bloomify.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+    private String avatarPath;
+
+
+}
