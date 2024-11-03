755645367cd5be61416cc4244a82ae88fa639455
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-03 14:16:28 +0300

    initial commit

---
+++ src/main/java/com/Bloomify/BloomifyBeApplication.java
@@ -0,0 +1,13 @@
+package com.Bloomify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomifyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomifyBeApplication.class, args);
+	}
+
+}
