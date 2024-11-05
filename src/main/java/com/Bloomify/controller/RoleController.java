6c5c907b974316e6335f050ba221ac964f8e2ab4
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-05 23:32:41 +0300

    FEAT:[BMF-6][Vadim]: kullanılmayan controller silindi

--- src/main/java/com/Bloomify/controller/RoleController.java
+++
@@ -1,9 +0,0 @@
-package com.Bloomify.controller;
-
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/api/role")
-public class RoleController  {}
