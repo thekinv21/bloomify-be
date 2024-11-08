3fd8738b965455b85ca44522690ca26c00671056
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-08 22:23:50 +0300

    FIX:[BMF-7][Vadim]: UserDto role to roleNames düzeltildi

--- src/main/java/com/Bloomify/dto/UserDto.java
+++ src/main/java/com/Bloomify/dto/UserDto.java
@@ -1,18 +1,19 @@
 package com.Bloomify.dto;
 import com.Bloomify.validation.CreateValidation;
 import com.Bloomify.validation.UpdateValidation;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 
 @Getter
 @Setter
-
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto extends BaseDto {
 
     @NotNull(groups = UpdateValidation.class)
@@ -36,5 +37,5 @@ public class UserDto extends BaseDto {
     public Boolean isActive;
 
     @NotNull(groups = {CreateValidation.class, UpdateValidation.class})
-    public List<RoleDto> roles;
+    private Set<String> roles;
 }
