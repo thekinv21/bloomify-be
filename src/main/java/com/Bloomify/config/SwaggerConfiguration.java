b65e2f735da6d125ba684772459187a7ca2b4815
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-09 14:12:24 +0300

    FEAT:[BMF-8][Vadim]: SwaggerConfiguration eklendi

---
+++ src/main/java/com/Bloomify/config/SwaggerConfiguration.java
@@ -0,0 +1,24 @@
+package com.Bloomify.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(description = "OpenApi documentation", title = "Bloomify", version = "1.0"),
+        security = {@SecurityRequirement(name = "bearerAuth")}
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+@Configuration
+public class SwaggerConfiguration {}
