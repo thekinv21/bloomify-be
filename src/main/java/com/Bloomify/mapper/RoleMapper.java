ad32192f42bea2ec516558cc66490f185d897339
Vadim Kiniabaev <thekinv21@gmail.com>
2024-11-09 14:57:32 +0300

    FEAT:[BMF-8][Vadim]: RoleMapper eklendi ve role dönüşümleri düzenlendi

--- src/main/java/com/Bloomify/mapper/RoleMapper.java
+++ src/main/java/com/Bloomify/mapper/RoleMapper.java
@@ -1,6 +1,7 @@
 package com.Bloomify.mapper;
 
 import com.Bloomify.dto.RoleDto;
+import com.Bloomify.dto.SelectDto;
 import com.Bloomify.model.Role;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -8,6 +9,10 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
 
+
+    @Mapping(source = "name", target = "label")
+    @Mapping(source = "id", target = "value")
+    SelectDto<Long> toSelectDto(Role role);
 
     RoleDto toDto(Role entity);
 
