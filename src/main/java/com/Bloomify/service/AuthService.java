27ff881c8ebcdb5cbca24592324eb7388978ee2f
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-30 01:40:56 +0300

    feat:[BMF-28][Vadim]: create new layer services for auth RefreshToekn and OtpValidation

--- src/main/java/com/Bloomify/service/AuthService.java
+++ src/main/java/com/Bloomify/service/AuthService.java
@@ -6,11 +6,15 @@ import com.Bloomify.dto.*;
 
 public interface AuthService {
 
-    TokenDto login(LoginRequest loginRequest);
+    TokenDto login(LoginDto loginDto);
 
     UserDto getMyself();
 
-    void logout(LogoutRequest logoutRequest);
+    void logout(LogoutDto logoutDto);
 
     UserDto register(UserDto dto);
+
+    TokenDto refreshToken(RefreshTokenDto refreshTokenDto);
+
+    boolean validateOtp(OtpValidateDto otpValidateDto);
 }
