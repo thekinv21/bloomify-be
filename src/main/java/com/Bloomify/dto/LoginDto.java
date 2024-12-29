ac79a42433ecc3487333d97bea1a336d8c28d39e
Vadim Kiniabaev <thekinv21@gmail.com>
2024-12-30 01:36:29 +0300

    fix:[BMF-28][Vadim]: fixed login and logout dtos

--- src/main/java/com/Bloomify/dto/LoginDto.java
+++ src/main/java/com/Bloomify/dto/LoginDto.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class LoginRequest {
+public class LoginDto {
     @NotBlank
     private String username;
     @NotBlank
